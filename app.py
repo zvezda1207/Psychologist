1f55db8d741c4a3f9666c582c0d62e335acbf86d
Ira Tka4eva <ira.tka4eva2011@yandex.ru>
2026-09-07 13:24:27 +0300

    Добавлена англоязычная версия

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ def home():
 def privacy():
     return render_template('privacy.html')
 
+@app.route('/en')
+def home_en():
+    return render_template('index_en.html')
+
+@app.route('/privacy_en')
+def privacy_en():
+    return render_template('privacy_en.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
