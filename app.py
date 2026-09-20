8fc6bb709d53e73c70f5d9818efd6e106e224b51
Ira Tka4eva <ira.tka4eva2011@yandex.ru>
2026-09-20 13:02:29 +0300

    Переместила favicon и apple-touch-icon

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
+# ---------- Маршруты страниц ----------
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -17,6 +19,23 @@ def home_en():
 @app.route('/privacy_en')
 def privacy_en():
     return render_template('privacy_en.html')
+
+# ---------- Иконки для браузера и поисковых систем ----------
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
+
+@app.route('/apple-touch-icon.png')
+def apple_touch_icon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'apple-touch-icon.png',
+        mimetype='image/png'
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
